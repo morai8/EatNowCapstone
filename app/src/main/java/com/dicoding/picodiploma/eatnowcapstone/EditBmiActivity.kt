dfcf3265cd2a171fe396057563f32b12bdc96b07
morai8 <miraitsuchiya21@gmail.com>
2023-05-28 18:30:10 +0700

    edit password + edit bmi + revisi navbar dikit

---
+++ app/src/main/java/com/dicoding/picodiploma/eatnowcapstone/EditBmiActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.picodiploma.eatnowcapstone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.picodiploma.eatnowcapstone.R
+
+class EditBmiActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_bmi)
+    }
+}
