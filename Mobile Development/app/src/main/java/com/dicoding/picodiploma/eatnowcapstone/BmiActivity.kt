88da12096d8b62d7bcc9df39b34e699da4e8bf66
morai8 <miraitsuchiya21@gmail.com>
2023-06-14 12:23:37 +0700

    FE masuk ke home

--- Mobile Development/app/src/main/java/com/dicoding/picodiploma/eatnowcapstone/BmiActivity.kt
+++ Mobile Development/app/src/main/java/com/dicoding/picodiploma/eatnowcapstone/BmiActivity.kt
@@ -1,17 +1,54 @@
 package com.dicoding.picodiploma.eatnowcapstone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dicoding.picodiploma.eatnowcapstone.databinding.ActivityBmiBinding
-import com.dicoding.picodiploma.eatnowcapstone.databinding.ActivitySignInBinding
+import android.content.SharedPreferences
+import android.preference.PreferenceManager
 
 class BmiActivity : AppCompatActivity() {
     private lateinit var binding: ActivityBmiBinding
+    private lateinit var sharedPreferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         binding = ActivityBmiBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
         PreferenceHelper.setFirstLogin(this, false)
+
+        binding.buttonSubmit.setOnClickListener {
+            val heightString = binding.heightField.text.toString()
+            val weightString = binding.weightField.text.toString()
+
+            val height = heightString.toIntOrNull()
+            val weight = weightString.toIntOrNull()
+
+            if (height != null && weight != null) {
+                val gender = when (binding.rgGender.checkedRadioButtonId) {
+                    R.id.rb_male -> "male"
+                    R.id.rb_female -> "female"
+                    else -> ""
+                }
+                sharedPreferences.edit().putString("gender", gender).apply()
+
+                sharedPreferences.edit().putInt("height", height).apply()
+
+                sharedPreferences.edit().putInt("weight", weight).apply()
+
+            } else {
+            }
+            val intentToHomeActivity = Intent(this,HomeActivity::class.java)
+            startActivity(intentToHomeActivity)
+        }
+
+    }
+    private fun retrieveSavedData() {
+        val gender = sharedPreferences.getString("gender", "")
+        val height = sharedPreferences.getInt("height", 0)
+        val weight = sharedPreferences.getInt("weight", 0)
+
     }
 }
--- Mobile Development/app/src/main/java/com/dicoding/picodiploma/eatnowcapstone/HomeActivity.kt
+++ Mobile Development/app/src/main/java/com/dicoding/picodiploma/eatnowcapstone/HomeActivity.kt
@@ -14,11 +14,12 @@ class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
 
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        setSupportActionBar(binding.toolbar)
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_home)
