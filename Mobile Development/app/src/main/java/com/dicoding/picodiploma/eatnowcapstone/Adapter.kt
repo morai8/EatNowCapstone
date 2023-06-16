107bcab2fb945677c67f19ecc0f1bc3cb903dd5c
morai8 <miraitsuchiya21@gmail.com>
2023-06-16 13:00:19 +0700

    Search Function work

--- Mobile Development/app/src/main/java/com/dicoding/picodiploma/eatnowcapstone/Adapter.kt
+++ Mobile Development/app/src/main/java/com/dicoding/picodiploma/eatnowcapstone/Adapter.kt
@@ -4,32 +4,34 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.dicoding.picodiploma.eatnowcapstone.model.RecipeItem
-import android.content.Context
-import android.view.View
-import android.widget.TextView
 import com.dicoding.picodiploma.eatnowcapstone.databinding.ItemRowIngredientsBinding
-import org.w3c.dom.Text
 
-class Adapter (val recipeItem:MutableList<RecipeItem>):RecyclerView.Adapter<RecipeViewHolder>()  {
+class Adapter(private val recipeItems: MutableList<RecipeItem>) :
+    RecyclerView.Adapter<Adapter.RecipeViewHolder>() {
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipeViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_row_ingredients,parent,false)
-        return RecipeViewHolder(view)
-    }
-
-    override fun getItemCount(): Int {
-        return recipeItem.size
+        val binding = ItemRowIngredientsBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return RecipeViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: RecipeViewHolder, position: Int) {
-        return holder.bindView(recipeItem[position])
+        holder.bindView(recipeItems[position])
     }
-}
-class RecipeViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
-    private val tvTitle : TextView = itemView.findViewById(R.id.tv_recipeName)
-    private val tvCalories : TextView = itemView.findViewById(R.id.tv_cal)
 
-    fun bindView(recipeItem : RecipeItem){
-        tvTitle.text = recipeItem.title
-        tvCalories.text = recipeItem.calories
+    override fun getItemCount(): Int {
+        return recipeItems.size
+    }
+
+    inner class RecipeViewHolder(private val binding: ItemRowIngredientsBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bindView(recipeItem: RecipeItem) {
+            binding.tvRecipeName.text = recipeItem.title
+            binding.tvCal.text = recipeItem.calories
+        }
     }
 }
--- Mobile Development/app/src/main/java/com/dicoding/picodiploma/eatnowcapstone/ui/dashboard/DashboardFragment.kt
+++ Mobile Development/app/src/main/java/com/dicoding/picodiploma/eatnowcapstone/ui/dashboard/DashboardFragment.kt
@@ -5,12 +5,12 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.dicoding.picodiploma.eatnowcapstone.Adapter
-import com.dicoding.picodiploma.eatnowcapstone.R
 import com.dicoding.picodiploma.eatnowcapstone.api.ApiRecipe
 import com.dicoding.picodiploma.eatnowcapstone.api.ApiService
 import com.dicoding.picodiploma.eatnowcapstone.databinding.FragmentDashboardBinding
@@ -18,10 +18,15 @@ import com.dicoding.picodiploma.eatnowcapstone.model.RecipeItem
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.util.Locale
 
 class DashboardFragment : Fragment() {
     private var _binding: FragmentDashboardBinding? = null
     private val binding get() = _binding!!
+    private lateinit var searchView: SearchView
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: Adapter
+    private var originalRecipeItems: MutableList<RecipeItem> = mutableListOf()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,10 +35,22 @@ class DashboardFragment : Fragment() {
     ): View {
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
-        val recyclerView: RecyclerView = binding.rvShowRecipe
+        recyclerView = binding.rvShowRecipe
+        searchView = binding.svIngredients
 
         val apiRecipe = ApiRecipe.buildService(ApiService::class.java)
         val call = apiRecipe.getRecipe()
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                filterList(newText)
+                return true
+            }
+        })
 
         call.enqueue(object : Callback<MutableList<RecipeItem>> {
             override fun onResponse(
@@ -41,9 +58,10 @@ class DashboardFragment : Fragment() {
                 response: Response<MutableList<RecipeItem>>
             ) {
                 if (response.isSuccessful) {
+                    originalRecipeItems = response.body()!!
                     recyclerView.apply {
                         layoutManager = LinearLayoutManager(requireContext())
-                        adapter = Adapter(response.body()!!)
+                        adapter = Adapter(originalRecipeItems)
                     }
                 }
             }
@@ -55,6 +73,22 @@ class DashboardFragment : Fragment() {
         })
 
         return root
+    }
+
+    private fun filterList(query: String?) {
+        if (query != null) {
+            val filteredList = originalRecipeItems.filter {
+                it.title?.toLowerCase(Locale.ROOT)!!.contains(query.toLowerCase(Locale.ROOT))
+            }.toMutableList()
+
+            if (filteredList.isEmpty()) {
+                Toast.makeText(requireContext(), "No Data Found", Toast.LENGTH_SHORT).show()
+            }
+
+            recyclerView.adapter = Adapter(filteredList)
+        } else {
+            recyclerView.adapter = Adapter(originalRecipeItems)
+        }
     }
 
     override fun onDestroyView() {
